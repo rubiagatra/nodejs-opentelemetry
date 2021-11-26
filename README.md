8f537d5fe587dfc67108c29a8360671e3591ad21
Doni Rubiagatra <doni@zero-one-group.com>
2021-11-26 11:37:32 +0700

    init express with opentelemetry

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nodejs-opentelemetry
+
 An example how OpenTelemetry used in Node.js
+
+# How to
+
+# Run the Zipkin exporter
+
+```sh
+docker run --rm -d -p 9411:9411 --name zipkin openzipkin/zipkin
+```
---
+++ tracing.js
@@ -0,0 +1,13 @@
+/* tracing.js */
+
+const opentelemetry = require("@opentelemetry/sdk-node");
+const {
+  getNodeAutoInstrumentations,
+} = require("@opentelemetry/auto-instrumentations-node");
+
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start();
